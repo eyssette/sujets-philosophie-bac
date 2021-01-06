7dea8e05ed7e7e6f37d6395ab706612bd9c23b36
eyssette <cedric.eyssette@gmail.com>
2021-01-06 13:58:39 +0100

    Add live update

--- js/parse_data-dissertations.js
+++ js/parse_data-dissertations.js
@@ -9,67 +9,63 @@ var table_body;
 table_start = "<table>";
 table_header = "<thead><tr>";
 table_end = "</table>";
-Papa.parse(
-  "https://raw.githubusercontent.com/eyssette/sujets_du_bac-philosophie/main/data/dissertations.tsv",
-  {
-    download: true,
-    header: false,
-    delimiter: "    ",
-    complete: function (results) {
-      rows = results.data;
-      title = rows[0][0].split("\t");
-      title.forEach((element) => {
-        table_header = table_header + "<th>" + element + "</th>";
-      });
-      table_header = table_header + "</tr></thead>";
-      rows = rows.splice(1, rows.length);
-      table_body = "<tbody>";
-      rows.forEach((element) => {
-        recherche = document.getElementById("recherche_dans_le_sujet").value;
-        table_body = table_body + "<tr>";
-        cellules = element[0].split("\t");
-        cellules.forEach((cell) => {
-          table_body = table_body + "<td>" + cell + "</td>";
-        });
-        table_body = table_body + "</tr>";
-      });
-      table_body = table_body + "</tbody>";
-      content = table_start + table_header + table_body + table_end;
-      document.getElementById("content").innerHTML = content;
-    }
-  }
-);
+Papa.parse("https://raw.githubusercontent.com/eyssette/sujets_du_bac-philosophie/main/data/dissertations.tsv", {
+	download: true,
+	header: false,
+	delimiter: "    ",
+	complete: function(results) {
+		rows = results.data;
+		title = rows[0][0].split("\t");
+		title.forEach((element) => {
+			table_header = table_header + "<th>" + element + "</th>";
+		});
+		table_header = table_header + "</tr></thead>";
+		rows = rows.splice(1, rows.length);
+		table_body = "<tbody>";
+		rows.forEach((element) => {
+			recherche = document.getElementById("recherche_dans_le_sujet").value;
+			table_body = table_body + "<tr>";
+			cellules = element[0].split("\t");
+			cellules.forEach((cell) => {
+				table_body = table_body + "<td>" + cell + "</td>";
+			});
+			table_body = table_body + "</tr>";
+		});
+		table_body = table_body + "</tbody>";
+		content = table_start + table_header + table_body + table_end;
+		document.getElementById("content").innerHTML = content;
+	}
+});
 let input = document.querySelector("input");
 input.oninput = handleInput;
 
 function handleInput(e) {
-  var search = input.value;
-  if(this.timer) {
+	var search = input.value;
+	if(this.timer) {
 		window.clearTimeout(this.timer);
 	}
 	this.timer = window.setTimeout(function() {
-  search_items = search.split("+");
-  pattern = "";
-  search_items.forEach((search_item) => {
-    pattern = pattern + "(?=.*" + search_item + ")";
-  });
-  regex = new RegExp(pattern);
-  table_body = "<tbody>";
-  rows.forEach((element) => {
-    cellules = element[0].split("\t");
-
-    if (cellules.toString().match(regex)) {
-      table_body = table_body + "<tr>";
-      cellules.forEach((cell) => {
-        table_body = table_body + "<td>" + cell + "</td>";
-      });
-      table_body = table_body + "</tr>";
-    }
-  });
-  table_body = table_body + "</tbody>";
-  content = table_start + table_header + table_body + table_end;
-  document.getElementById("content").innerHTML = content;
-}, 150);
+		search_items = search.split("+");
+		pattern = "";
+		search_items.forEach((search_item) => {
+			pattern = pattern + "(?=.*" + search_item + ")";
+		});
+		regex = new RegExp(pattern);
+		table_body = "<tbody>";
+		rows.forEach((element) => {
+			cellules = element[0].split("\t");
+			if(cellules.toString().match(regex)) {
+				table_body = table_body + "<tr>";
+				cellules.forEach((cell) => {
+					table_body = table_body + "<td>" + cell + "</td>";
+				});
+				table_body = table_body + "</tr>";
+			}
+		});
+		table_body = table_body + "</tbody>";
+		content = table_start + table_header + table_body + table_end;
+		document.getElementById("content").innerHTML = content;
+	}, 150);
 }
 
 function delay(fn, ms) {
--- js/parse_data-explications.js
+++ js/parse_data-explications.js
@@ -9,58 +9,56 @@ var table_body;
 table_start = "<table>";
 table_header = "<thead><tr>";
 table_end = "</table>";
-Papa.parse(
-  "https://raw.githubusercontent.com/eyssette/sujets_du_bac-philosophie/main/data/explications.tsv",
-  {
-    download: true,
-    header: false,
-    delimiter: "    ",
-    complete: function (results) {
-      rows = results.data;
-      title = rows[0][0].split("\t");
-      title.forEach((element) => {
-        table_header = table_header + "<th>" + element + "</th>";
-      });
-      table_header = table_header + "</tr></thead>";
-      rows = rows.splice(1, rows.length);
-      table_body = "<tbody>";
-      rows.forEach((element) => {
-        recherche = document.getElementById("recherche_dans_le_sujet").value;
-        table_body = table_body + "<tr>";
-        cellules = element[0].split("\t");
-        cellules.forEach((cell) => {
-          table_body = table_body + "<td>" + cell + "</td>";
-        });
-        table_body = table_body + "</tr>";
-      });
-      table_body = table_body + "</tbody>";
-      content = table_start + table_header + table_body + table_end;
-      document.getElementById("content").innerHTML = content;
-    }
-  }
-);
+Papa.parse("https://raw.githubusercontent.com/eyssette/sujets_du_bac-philosophie/main/data/explications.tsv", {
+	download: true,
+	header: false,
+	delimiter: "    ",
+	complete: function(results) {
+		rows = results.data;
+		title = rows[0][0].split("\t");
+		title.forEach((element) => {
+			table_header = table_header + "<th>" + element + "</th>";
+		});
+		table_header = table_header + "</tr></thead>";
+		rows = rows.splice(1, rows.length);
+		table_body = "<tbody>";
+		rows.forEach((element) => {
+			recherche = document.getElementById("recherche_dans_le_sujet").value;
+			table_body = table_body + "<tr>";
+			cellules = element[0].split("\t");
+			cellules.forEach((cell) => {
+				table_body = table_body + "<td>" + cell + "</td>";
+			});
+			table_body = table_body + "</tr>";
+		});
+		table_body = table_body + "</tbody>";
+		content = table_start + table_header + table_body + table_end;
+		document.getElementById("content").innerHTML = content;
+	}
+});
 var input = document.getElementById("recherche_dans_le_sujet");
-
+input.oninput = handleInput;
 let checker = (arr, target) => target.every((v) => arr.includes(v));
 
-input.addEventListener("keyup", function (event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    search = event.target.value;
-    search_items = search.split("+");
-    table_body = "<tbody>";
-    rows.forEach((element) => {
-      cellules = element[0].split("\t");
-
-      if (checker(cellules[4], search_items)) {
-        table_body = table_body + "<tr>";
-        cellules.forEach((cell) => {
-          table_body = table_body + "<td>" + cell + "</td>";
-        });
-        table_body = table_body + "</tr>";
-      }
-    });
-    table_body = table_body + "</tbody>";
-    document.getElementsByTagName("tbody")[0].innerHTML = table_body;
-  }
-});
+function handleInput(e) {
+	var search = input.value;
+	if(this.timer) {
+		window.clearTimeout(this.timer);
+	}
+	this.timer = window.setTimeout(function() {
+		search_items = search.split("+");
+		table_body = "<tbody>";
+		rows.forEach((element) => {
+			cellules = element[0].split("\t");
+			if(checker(cellules[4], search_items)) {
+				table_body = table_body + "<tr>";
+				cellules.forEach((cell) => {
+					table_body = table_body + "<td>" + cell + "</td>";
+				});
+				table_body = table_body + "</tr>";
+			}
+		});
+		table_body = table_body + "</tbody>";
+		document.getElementsByTagName("tbody")[0].innerHTML = table_body;
+	}, 200);
+}
