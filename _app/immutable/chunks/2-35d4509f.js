180b18647fe50205de23c4b848d2e77f1247540e
eyssette <cedric.eyssette@gmail.com>
2023-01-26 12:35:24 +0100

    Updates

---
+++ _app/immutable/chunks/2-35d4509f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-b02f9ddd.js";export{t as component};
--- _app/immutable/chunks/2-dc7fb8a1.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-b9e18d12.js";export{t as component};
